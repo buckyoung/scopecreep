4272ca0dd48ad78adc3909b8be19d1ef48aec281
Nathan Donato <nathanjdonato@gmail.com>
2016-05-21 19:12:55 -0400

    Add controller support, invader ship, and walls

---
+++ Assets/GetInfo.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GetInfo : MonoBehaviour {
+	public Transform target;
+	Camera camera;
+
+	void Start() {
+		camera = GetComponent<Camera>();
+	}
+
+	void Update() {
+		Vector3 screenPos = camera.WorldToScreenPoint(target.position);
+		Debug.Log("target is " + screenPos.x + " pixels from the left");
+	}
+}
