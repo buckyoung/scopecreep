9786cbcbc5d081db76094c7c9b15d72d762047a1
Buck Young <buckcyoung@gmail.com>
2016-05-22 13:03:59 -0400

    TABS

--- Assets/Scripts/DrawWalls.cs
+++ Assets/Scripts/DrawWalls.cs
@@ -4,18 +4,18 @@ public class DrawWalls : MonoBehaviour {
 	private GameObject leftWall;
 	private GameObject rightWall;
 
-    void Start() {
-        leftWall = transform.Find("LeftWall").gameObject;
-        rightWall = transform.Find("RightWall").gameObject;
-    }
+	void Start() {
+		leftWall = transform.Find("LeftWall").gameObject;
+		rightWall = transform.Find("RightWall").gameObject;
+	}
 
-    void Update() {
-        // Get camera borders
-        float leftBorder = transform.InverseTransformPoint(Camera.main.ViewportToWorldPoint(new Vector3(0, 0, 0))).x;
-        float rightBorder = transform.InverseTransformPoint(Camera.main.ViewportToWorldPoint(new Vector3(1, 0, 0))).x;
-        
+	void Update() {
+		// Get camera borders
+		float leftBorder = transform.InverseTransformPoint(Camera.main.ViewportToWorldPoint(new Vector3(0, 0, 0))).x;
+		float rightBorder = transform.InverseTransformPoint(Camera.main.ViewportToWorldPoint(new Vector3(1, 0, 0))).x;
+		
 		// Dynamically set the walls' positions
 		leftWall.transform.localPosition = new Vector3(leftBorder - 1.0f, leftWall.transform.localPosition.y, 0);
 		rightWall.transform.localPosition = new Vector3(rightBorder + 1.0f, rightWall.transform.localPosition.y, 0);
-    }
+	}
 }
