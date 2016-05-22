b92f47a70631ed7d97c8a1882e6140048d2237b6
Nathan Donato <nathanjdonato@gmail.com>
2016-05-22 11:03:35 -0400

    Clean up spacing, comments, and unused import

--- Assets/Scripts/DrawWalls.cs
+++ Assets/Scripts/DrawWalls.cs
@@ -1,5 +1,4 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class DrawWalls : MonoBehaviour {
     private Transform leftWall;
@@ -10,7 +9,7 @@ public class DrawWalls : MonoBehaviour {
         rightWall = transform.Find("RightWall").gameObject.transform;
     }
 
-	void Update() {
+    void Update() {
         // Get camera borders
         Vector3 viewPos = Camera.main.WorldToViewportPoint(transform.position);
         float dist = (transform.position - Camera.main.transform.position).z;
@@ -20,7 +19,5 @@ public class DrawWalls : MonoBehaviour {
         // Dynamically set the walls' positions
         leftWall.localPosition = new Vector3(leftBorder - 0.5f, leftWall.localPosition.y, leftWall.localPosition.z);
         rightWall.localPosition = new Vector3(rightBorder + 0.5f, rightWall.localPosition.y, rightWall.localPosition.z);
-
-        // NJD TODO: Consider altering the ceiling's scale (currently hard coded to 50 units long, which should work for every screen size.
     }
 }
