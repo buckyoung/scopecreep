fba48ae5fa49b35237b536923672d5d027bb69e2
Nathan Donato <nathanjdonato@gmail.com>
2016-05-22 18:34:16 -0400

    Moved prefabs to Resources folder because it's needed for Instantiate()

---
+++ Assets/Scripts/Collection.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Collection : MonoBehaviour {
+	void OnTriggerEnter(Collider other) {
+		if (other.CompareTag("Collectible")) {
+			Destroy(other.gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/ResourceSpawner.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResourceSpawner : MonoBehaviour {
+	void Update () {
+		if (Input.GetKeyDown("p")) {
+			GameObject resource = (GameObject)Instantiate(Resources.Load("ResourcePrefab"), new Vector3(0, 52, 0), Quaternion.identity);
+			resource.transform.localScale = new Vector3 (1, 1, 1);
+		}
+	}
+}
