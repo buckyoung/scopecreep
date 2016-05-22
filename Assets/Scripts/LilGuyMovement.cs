a57eec8a2d13faaa9613b1067d5c32c03b3176d9
Buck Young <buckcyoung@gmail.com>
2016-05-21 21:23:44 -0400

    Merged the projects, mothership scaffolding and movement, lil guy fuck yeah

---
+++ Assets/Scripts/LilGuyMovement.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LilGuyMovement : MonoBehaviour {
+	public int playerNum = 1;
+
+	public float speed = 10.0f;
+
+	void Update() {
+		var move = new Vector3(Input.GetAxis("P" + playerNum + "_X_AXIS"), Input.GetAxis("P" + playerNum + "_Y_AXIS"), 0);
+		transform.position += move * speed * Time.deltaTime;
+	}
+}
---
+++ Assets/Scripts/MothershipMovement.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MothershipMovement : MonoBehaviour {
+	public int playerNum = 1;
+
+	public float speed = 1.0f;
+
+	void Update() {
+		var move = new Vector3(Input.GetAxis("P" + playerNum + "_X_AXIS"), Input.GetAxis("P" + playerNum + "_Y_AXIS"), 0);
+		transform.position += move * speed * Time.deltaTime;
+	}
+}
