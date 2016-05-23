b1a36808d7193cf95da7686836291ed276d3aa5d
Buck Young <buckcyoung@gmail.com>
2016-05-23 01:58:47 -0400

    Hide player upon interacting with lil guy module

--- Assets/Scripts/Modules/LilGuyModule.cs
+++ Assets/Scripts/Modules/LilGuyModule.cs
@@ -2,5 +2,19 @@
 using System.Collections;
 
 public class LilGuyModule : Module {
+	private int previousActivePlayerId = 0; 
 
+	void Update() {
+		base.Update();
+
+		if (activePlayerId > 0) {
+			int index = activePlayerId - 1;
+			players[index].GetComponent<SpriteRenderer>().enabled = false;
+			previousActivePlayerId = activePlayerId;
+		} else if (previousActivePlayerId > 0) {
+			int index = previousActivePlayerId - 1;
+			players[index].GetComponent<SpriteRenderer>().enabled = true;
+			previousActivePlayerId = 0;
+		}
+	}
 }
