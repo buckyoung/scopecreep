eaf30f92bc5664737875143d9f304a438def3d8e
Buck Young <buckcyoung@gmail.com>
2016-05-25 02:02:06 -0400

    Implementing jumping and ground tags

--- Assets/Scripts/Movement/PlayerMovement.cs
+++ Assets/Scripts/Movement/PlayerMovement.cs
@@ -3,21 +3,35 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
 	public bool isAtModule = false;
+	public int jumpHeight = 30;
 	public int playerId = 1;
 	public Rigidbody2D rigidbody2D;
 	public float speed = 150.0f;
+
+	private bool canJump = true;
 	
 	void Start() {
 		rigidbody2D = GetComponent<Rigidbody2D>();
 
 		// Ignore collisions between PlayerCharacters
-		Physics2D.IgnoreLayerCollision(8, 8);
+		Physics2D.IgnoreLayerCollision(gameObject.layer, gameObject.layer);
 	}
 
 	void FixedUpdate() {
 		if (!isAtModule) {
-			var movement = new Vector2(Input.GetAxis(playerId + "_AXIS_X"), Input.GetAxis(playerId + "_AXIS_Y"));
+			var movement = new Vector2(Input.GetAxis(playerId + "_AXIS_X"), 0);
 			rigidbody2D.AddRelativeForce(movement * speed * Time.deltaTime);
+
+			if (Input.GetButtonDown(playerId + "_BTN_A") && canJump) {
+				rigidbody2D.AddRelativeForce(new Vector2(0, jumpHeight) * Time.deltaTime, ForceMode2D.Impulse);
+				canJump = false;
+			}
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D col) {
+		if (col.gameObject.CompareTag("Ground")) {
+			canJump = true;
 		}
 	}
 }
