dc76d53709812ce1205803e936fba2564c4749d7
Nathan Donato <nathanjdonato@gmail.com>
2016-05-28 17:56:10 -0400

    Adds some enemy things

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace ScopeCreep.Enemy {
+	public class Tower : MonoBehaviour {
+		public int hitPoints = 50;
+
+		// Use this for initialization
+		void Start () {
+
+		}
+		
+		// Update is called once per frame
+		void Update () {
+		
+		}
+	}
+}
---
+++ Assets/Scripts/Enemy/Gun.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Enemy.Gun {
+	public class Gun : MonoBehaviour {
+		public GameObject target = null;
+		public int ammo = 100;
+		public float speed = 5;
+
+		// Use this for initialization
+		void Start() {
+		}
+		
+		// Update is called once per frame
+		void Update() {
+			if (target != null) {
+				Vector3 vectorToTarget = target.transform.position - transform.position;
+				float angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg;
+				Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+				transform.rotation = Quaternion.SlerpUnclamped(transform.rotation, q, Time.deltaTime *speed);
+			}
+		}
+
+		private void OnTriggerEnter2D(Collider2D col) {
+			if ( col.gameObject.layer == LayerMask.NameToLayer("Childship") ) {
+				target = col.gameObject;
+			}
+		}
+
+		private void OnTriggerExit2D(Collider2D col) {
+			if ( col.gameObject.layer == LayerMask.NameToLayer("Childship") ) {
+				target = null;
+			}
+		}
+	}
+}
