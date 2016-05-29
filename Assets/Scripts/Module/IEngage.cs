ee0369272a8aa17c605a90073d57006ad44d214c
Buck Young <buckcyoung@gmail.com>
2016-05-29 15:32:41 -0400

    whoops forgot to add these

---
+++ Assets/Scripts/Module/IEngage.cs
@@ -0,0 +1,8 @@
+﻿using ScopeCreep.Player;
+
+namespace ScopeCreep.Module {
+	public interface IEngage {
+		void engage(ShipModule module, PlayerInfo player);
+		void disengage(ShipModule module, PlayerInfo player);
+	}
+}
