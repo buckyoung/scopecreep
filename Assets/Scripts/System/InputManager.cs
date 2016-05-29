605e04b994c11e986bde949ede83abf4cc280de5
Buck Young <buckcyoung@gmail.com>
2016-05-29 16:55:03 -0400

    R-529-2 Move system scripts into system object. Create input manager empty class

---
+++ Assets/Scripts/System/InputManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace ScopeCreep.System {
+	public class InputManager : MonoBehaviour {
+
+		// Use this for initialization
+		void Start () {
+		
+		}
+		
+		// Update is called once per frame
+		void Update () {
+		
+		}
+	}
+}
