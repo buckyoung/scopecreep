2d8b9065fd71bcff7a4a69f87b69e8600e56c704
Buck Young <buckcyoung@gmail.com>
2016-05-29 15:43:51 -0400

    R-529-1 Halfway to center hack

--- Assets/Scripts/Module/EngageSitDown.cs
+++ Assets/Scripts/Module/EngageSitDown.cs
@@ -10,11 +10,25 @@ namespace ScopeCreep.Module {
 		private Vector4 originalColor;
 
 		public void engage(ShipModule module, PlayerInfo player) {
-			
+			moveHalfwayTowardCenter(player, module);
 		}
 
 		public void disengage(ShipModule module, PlayerInfo player) {
 			
+		}
+
+		private void moveHalfwayTowardCenter(PlayerInfo player, ShipModule module) {
+			float halfwayX = (module.transform.position.x - player.transform.position.x) / 2;
+			float newX = player.transform.position.x + halfwayX;
+
+			// Teleport player toward center of module and ensure they keep their original y & z coordinate
+			// This HACK ensures the player is touching the module and hasnt slid past when engaging
+			// and should be removed at some point TODO BUCK
+			player.transform.position = new Vector3(
+				newX,
+				player.transform.position.y,
+				player.transform.position.z
+			); 
 		}
 	}
 }
