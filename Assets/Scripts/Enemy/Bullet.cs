eb86c778deb701d622f9dbee602410417a142d04
Nathan Donato <nathanjdonato@gmail.com>
2016-05-29 20:57:13 -0400

    Unify default damage

--- Assets/Scripts/Enemy/Bullet.cs
+++ Assets/Scripts/Enemy/Bullet.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 namespace ScopeCreep.Enemy.Bullet {
 	public class Bullet : MonoBehaviour {
-		public int damageToDeal = 1;
+		public int damageToDeal = 2;
 
 		void OnCollisionEnter2D(Collision2D col) {
 			if ( col.gameObject.layer != LayerMask.NameToLayer("Enemy") ) {
