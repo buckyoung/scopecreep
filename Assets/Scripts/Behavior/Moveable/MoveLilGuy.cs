43ad73a62b7eef966a8d3195038831828367d7e8
Buck Young <buckcyoung@gmail.com>
2016-06-03 22:33:03 -0400

    R-530-20 MoveLilGuy + lilGuyMovement refactor

--- Assets/Scripts/Behavior/Moveable/MoveLilGuy.cs
+++ Assets/Scripts/Behavior/Moveable/MoveLilGuy.cs
@@ -1,15 +1,41 @@
 ﻿using UnityEngine;
 using System.Collections;
+using ScopeCreep;
 
-public class MoveLilGuy : MonoBehaviour {
+namespace ScopeCreep.Behavior {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	[RequireComponent (typeof (Rigidbody2D))]
+
+	public class MoveLilGuy : MonoBehaviour, IMoveable {
+		public float speed = 1.0f;
+
+		private Rigidbody2D rb2D;
+		private Module.LilGuy.Module lilGuyModule;
+
+		// Events
+		public delegate void LilGuyMovementEvent(MoveLilGuy eventObject, float totalForce);
+		public static event LilGuyMovementEvent onLilGuyMovement;
+
+		void Start() {
+			rb2D = GetComponent<Rigidbody2D>();
+			lilGuyModule = GameObject.Find("LilGuyModule").GetComponent<Module.LilGuy.Module>();
+
+			Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer("PlayerCharacter"), LayerMask.NameToLayer("Childship"));
+		}
+
+		/*
+		 * User Functions
+		 */
+
+		public void move() {
+			int activePlayerId = lilGuyModule.activePlayerId;
+
+			Vector2 movement = new Vector2(Input.GetAxis(activePlayerId + "_AXIS_X"), Input.GetAxis(activePlayerId + "_AXIS_Y"));
+			Vector2 totalForce = movement * speed * Time.deltaTime;
+
+			rb2D.AddRelativeForce(totalForce);
+
+			onLilGuyMovement(this, totalForce.magnitude);
+		}
 	}
 }
