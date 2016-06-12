7da746e327ec81aae12ca5add7af73fa752ca7d4
Buck Young <buckcyoung@gmail.com>
2016-06-12 01:47:55 -0400

    SC-612-23 Resource spawner now spawns all resource types

--- Assets/Scripts/System/ResourceSpawner.cs
+++ Assets/Scripts/System/ResourceSpawner.cs
@@ -4,12 +4,19 @@ using ScopeCreep;
 
 namespace ScopeCreep.System {
 	public class ResourceSpawner : MonoBehaviour {
-		public string resourceName = "Resource/SpaceDollar";
+		private string[] resourceName = new string[4];
 
 		private GameObject mothership;
 
 		void Start() {
 			mothership = GameObject.Find("Mothership");
+
+			// Hardcoded Debug
+			resourceName[0] = "Collectible/Ammo";
+			resourceName[1] = "Collectible/Fuel";
+			resourceName[2] = "Collectible/SpaceDollar";
+			resourceName[3] = "Collectible/Thiamin";
+			// End Debug
 
 			subscribe();
 		}
@@ -20,9 +27,13 @@ namespace ScopeCreep.System {
 		private void subscribe() {
 			// Drop resource under ship (DEBUG)
 			ButtonEventManager.onDebugButtonDown += (eventObject, playerId) => {
+				string name = resourceName[Mathf.RoundToInt(Random.value * 3)];
+				Vector3 position = mothership.transform.position - (mothership.transform.up * 2.0f);
+				position.z = 50;
+
 				GameObject resource = (GameObject)Instantiate(
-					Resources.Load(resourceName), 
-					mothership.transform.position - (mothership.transform.up * 2.0f), 
+					Resources.Load(name), 
+					position,
 					Quaternion.identity
 				);
 
