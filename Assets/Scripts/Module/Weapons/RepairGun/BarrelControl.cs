93bd1f5e98ab92042d56cb981c606fca25c8289b
Nathan Donato <nathanjdonato@gmail.com>
2016-06-14 19:09:33 -0400

    Add friendly projectiles

---
+++ Assets/Scripts/Module/Weapons/RepairGun/BarrelControl.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using ScopeCreep.System;
+using ScopeCreep.Behavior;
+
+namespace ScopeCreep.Module.RepairGun { 
+	public class BarrelControl : MonoBehaviour {
+		private RepairGun.Module repairGunModule;
+		private Behavior.IShoot shootBehavior;
+
+		void Start() {
+			repairGunModule = transform.parent.parent.GetComponentInChildren<Module>();
+			shootBehavior = this.GetComponent<Behavior.IShoot>();
+			subscribe();
+		}
+
+		/*
+		 * User Functions
+		 */
+
+		void subscribe() {
+			// The player wants to jump
+			ButtonEventManager.onAButtonDown += (eventObject, playerId) => {
+				if (playerId == repairGunModule.activePlayerId) {
+					shootBehavior.shoot();
+				}
+			};
+		}
+	}
+}
